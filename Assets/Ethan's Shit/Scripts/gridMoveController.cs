9d2cc41c0075d3f6f9df46706cd1f3d4da77191e
HyperContrlr <hypercontrlrtwitch@gmail.com>
2024-10-08 21:16:22 -0700

    Did some tilemap stuff, may need to organize art folder

--- Assets/Ethan's Shit/Scripts/gridMoveController.cs
+++ Assets/Ethan's Shit/Scripts/gridMoveController.cs
@@ -1,12 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class gridMoveController : MonoBehaviour
 {
+    private float currentMoveSpeed;
     public float moveSpeed;
+    public float sprintSpeed;
     public Transform movePoint;
     public LayerMask moveStopper;
+    public bool hasSprintShoes = false;
+    
+    public KeyCode sprint;
+    public KeyCode interact;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,16 +23,41 @@ public class gridMoveController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+        if (Input.GetKey(sprint) && hasSprintShoes)
+        {
+            currentMoveSpeed = sprintSpeed;
+        }
+        else
+        {
+            currentMoveSpeed = moveSpeed;
+        }
+        
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, currentMoveSpeed * Time.deltaTime);
         if(Vector3.Distance(transform.position, movePoint.position) <= .05)
         {
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
             {
-                movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, moveStopper))
+                {
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                }
             }
             if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
             {
-                movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, moveStopper))
+                {
+                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                }
+            }
+        }
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (Input.GetKeyDown(interact))
+        {
+            if (collision.gameObject.CompareTag("NPC"))
+            {
+                Console.WriteLine("WORKS");
             }
         }
     }
