97e94d7aadadf17ad3f833644728cb262e196d1b
Ethan Kellar <hypercontrlrtwitch@gmail.com>
2024-10-10 09:52:38 -0700

    New Interaction system, check for errors later

--- Assets/Ethan's Shit/Scripts/gridMoveController.cs
+++ Assets/Ethan's Shit/Scripts/gridMoveController.cs
@@ -61,4 +61,30 @@ public class gridMoveController : MonoBehaviour
             }
         }
     }
+/* Try this:
+public void OnCollisionEnter2D(Collision2D collision)
+{
+    if (Input.GetKeyDown(interact))
+    {
+        if (collision.gameObject.CompareTag("NPC"))
+        {
+            // Handle NPC interaction here
+            // For example, you could open a dialogue window or start a battle
+            Debug.Log("Interacting with NPC: " + collision.gameObject.name);
+        }
+        else if (collision.gameObject.CompareTag("Item"))
+        {
+            // Handle item interaction here
+            // For example, you could add the item to the player's inventory or use it immediately
+            Debug.Log("Interacting with item: " + collision.gameObject.name);
+        }
+        else if (collision.gameObject.CompareTag("Encounter"))
+        {
+            // Handle encounter interaction here
+            // For example, you could start a random encounter with a wild Pokemon
+            Debug.Log("Encountering wild Pokemon: " + collision.gameObject.name);
+        }
+    }
+}
+*/
 }
